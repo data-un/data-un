9b89b629fc67d789e888c770e3dbfa1964afe266
David Ricardo Pedraza Silva <63820927+Whippoorwil@users.noreply.github.com>
2021-04-24 22:26:02 -0500

    Update script.py

--- dat-un/src/dat/un/script/script.py
+++ dat-un/src/dat/un/script/script.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def generate(columnas, filas):
     columns = [ 'columna {}'.format(i) for i in range(columnas)]
-    diccionario = { i: np.random.randint(10000, size = filas) if i != 'columna 1' 
+    diccionario = { i: np.random.randint(100000000, size = filas) if i != 'columna 1' 
     else np.random.randint(2,size = filas) for i in columns}
     return pd.DataFrame.from_dict(diccionario)
 
