c34d57c2889d4b357fd3c54fd8003b4e6b63bb56
david <drpedrazas@unal.edu.co>
2021-04-25 14:10:21 -0500

    fixed case indexing

--- dat-un/src/dat/un/script/script.py
+++ dat-un/src/dat/un/script/script.py
@@ -3,9 +3,13 @@ import numpy as np
 
 def generate(columnas, filas):
     columns = [ "Caso", "Edad", "Fecha inicio", "Fecha Dx", "Sexo", "Ciudad", "Localidad", "Ubicación", "Estado", "tipo.contagio"]
-    diccionario = { i: np.random.randint(100000000, size = filas) if i != 'Estado' 
-    else np.random.randint(2,size = filas) for i in columns}
+    diccionario = {
+        i : np.arange(filas) + 1 if i == 'Caso' 
+        else np.random.randint(2, size = filas) if i == 'Estado'
+        else np.random.randint(10**8, size = filas) 
+        for i in columns 
+    }
     return pd.DataFrame.from_dict(diccionario)
 
 for i in range(1,8):
-    generate(4,10**i).to_json('tests{}.json'.format(10**i))
+    generate(4,10**i).to_csv('tests{}.csv'.format(10**i))
